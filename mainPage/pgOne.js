fa26f18e8ce5ca4e9c67cdeb22ca7d2c2121f424
Akshara Debnath <akshara.debnath@ryerson.ca>
2021-08-21 00:32:54 -0400

    main page

---
+++ mainPage/pgOne.js
@@ -0,0 +1,33 @@
+  function play(soundName){
+    let sound = new Audio("../IgnitionHack2021/mainPage/" + soundName + ".mp3");
+    sound.play();
+    sound.volume = 0.2;
+  }
+  
+  function pause(soundName){
+    let sound = new Audio("../IgnitionHack2021/mainPage/" + soundName + ".mp3");
+    sound.onpause();
+    sound.currentTime=0;
+  }
+
+  function hoppy() {
+    if (!document.getElementById("checkbox").checked) {
+        alert("Please check few boxes for the best result");
+    }
+    else
+    {
+        window.open("https://www.youtube.com/watch?v=XZeqK7IMrXU", "_blank");
+        // window.location.href = "";
+    }
+  }
+
+  function sadge() {
+    if (!document.getElementById("checkbox").checked) {
+        alert("Please check few boxes for the best result");
+    }
+    else
+    {
+        window.open("https://www.youtube.com/watch?v=XZeqK7IMrXU", "_blank");
+        // window.location.href = "";
+    }
+  }
