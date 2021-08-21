3fc0cb215303b5439d1f0d811747211d9be17329
Akshara Debnath <akshara.debnath@ryerson.ca>
2021-08-21 16:04:01 -0400

    spotify

---
+++ hoppy/songs.js
@@ -0,0 +1,50 @@
+let allMusic = [
+    {
+      name: "Medicine",
+      artist: "Artist vs Poet",
+      img : "Medicine",
+      src: 'Medicine',
+    },
+    {
+      name: "Now you know",
+      artist: "Jonah Green",
+      img : "nowYouKnow",
+      src: 'nowYouKnow',
+    },
+    {
+      name: "Boy like Me",
+      artist: "New Medicine",
+      img : "boyLikeMe",
+      src: 'boyLikeMe',
+    },
+    {
+      name: "Doctor",
+      artist: "Truslow",
+      img : "Doctor",
+      src: 'Doctor',
+    },
+    {
+      name: "Bad Habits",
+      artist: "The Federal Empire",     
+      img : "badHabits",
+      src: 'badHabits',
+    },
+    {
+      name: "Everywhere I go",
+      artist: "New Politics",
+      img : "everywhereIgo",
+      src: 'everywhereIgo',
+    },
+    {
+      name: "Popstar",
+      artist: "New Hollow",
+      img : "popstar",
+      src: 'popstar',
+    },
+    {
+      name: "Aristrocrat",
+      artist: "New Politics",
+      img : "aristocrat",
+      src: 'aristocrat',
+    },
+];
