e925f95137e69610ad7ae4d6a02ebb2f6341f4c6
Akshara Debnath <akshara.debnath@ryerson.ca>
2021-08-22 00:08:31 -0400

    chatbot

--- hoppy/AI.js
+++ hoppy/AI.js
@@ -1,9 +1,15 @@
 function talk(){
     var know ={
         "Hi":"Hello",
-        "How Are You?":"Great !",
+        "How are you?":"Great !",
         "Bye":"Have A Nice Day !",
-        "Hello":"Hi , Whats Up"
+        "Hello":"Hi , Whats Up",
+        "I am sad": "Its gunna be alright!",
+        "What is pi?" : "3.14",
+        "What the circumference of a circle?" : "3.14 x diameter",
+        "Area formula of a triangle" : "(base x height) /2",
+        "Squareroot of a neg number" : "doesn't exist",
+        "Should I attend class tomorrow?" : "YES! if possible"
     };
     var user = document.getElementById('userBox').value;
     document.getElementById('chatLog').innerHTML = user + "<br>";
